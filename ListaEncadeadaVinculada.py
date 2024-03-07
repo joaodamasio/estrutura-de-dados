9c7bccc7370f2af2e0866779f87e3512f81590c6
joaodamásio <joaovitormessiasdamasio@gmail.com>
2024-03-07 10:37:10 -0300

    alterando primeiro para head e adicionando o tail

--- ListaEncadeadaVinculada.py
+++ ListaEncadeadaVinculada.py
@@ -1,50 +1,52 @@
 class Paciente:
-    def __init__(self, nome_paciente, id_paciente, estado_saude) -> None:
+    def __init__(self, nome_paciente, id, estado_saude) -> None:
         self.nome_paciente = nome_paciente
-        self.id_paciente = id_paciente
+        self.id = id
         self.estado_saude = estado_saude
         self.proximo = None
         
 class ListaDePaciente:
     def __init__(self) -> None:
-        self.primeiro = None
+        self.head = None
+        self.tail = None
         
     def adicionar_paciente(self, nome, id, estado):
         novo_paciente = Paciente(nome, id, estado)
         
-        if self.primeiro is None:
-            self.primeiro = novo_paciente
+        if self.head is None:
+            self.head = novo_paciente
+            self.tail = novo_paciente
             
         else:
-            atual = self.primeiro
-            while atual.proximo:
-                atual = atual.proximo
-            atual.proximo = novo_paciente
+            self.tail.proximo = novo_paciente
+            self.tail = novo_paciente
             
     def remover_paciente(self, id):
-        atual = self.primeiro
-        anterior = None
-        while atual:
-            if atual.id_paciente == id:
-                if anterior:
-                    anterior.proximo = atual.proximo
-                else:
-                    self.primero = atual.proximo
-                    
-                return True
-            anterior = atual
-            atual = atual.proximo
-        return False
+        if self.head is None:
+            return
+        elif self.head.id == id:
+            self.head = self.head.proximo
+            if self.head is None:
+                self.tail = None
+            return
+        else:
+            paciente_atual = self.head
+            while paciente_atual.proximo is not None:
+                if paciente_atual.proximo.id == id:
+                    paciente_atual.proximo = paciente_atual.proximo.proximo
+                    if paciente_atual.proximo is None:
+                        self.tail = paciente_atual
+                        return
+                    paciente_atual = paciente_atual.proximo
     
     def listar_pacientes(self):
-        atual = self.primeiro
-        print('Lista de pacientes:')
-        while atual:
-            print('Nome:', atual.nome_paciente)
-            print('Id:', atual.id_paciente)
-            print('Estado de saúde:', atual.estado_saude)
-            print('---------------------------------')
-            atual = atual.proximo
+        if self.head is None:
+            print('Não há pacientes nesta lista')
+        else:
+            paciente_atual = self.head
+            while paciente_atual is not None:
+                print(f'Nome: {paciente_atual.nome_paciente}, ID: {paciente_atual.id}, Estado de saúde: {paciente_atual.estado_saude}')
+                paciente_atual = paciente_atual.proximo
             
             
 
