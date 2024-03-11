ec44e36e5493e5649949b3485020e49d318890dd
joaodamásio <joaovitormessiasdamasio@gmail.com>
2024-03-11 09:54:41 -0300

    correcao

--- Fila.py
+++ Fila.py
@@ -1,47 +1,39 @@
 class Pedido:
-    def __init__(self, nmr_pedido, nome_cliente, itens_pedido, valor_total):
-        self.nmr_pedido = nmr_pedido
-        self.nome_cliente = nome_cliente
-        self.itens_pedido = itens_pedido
-        self.valor_total = valor_total
+    def __init__(self,numero, nome, prato):
+        self.numero = numero
+        self.nome = nome
+        self.prato = prato
         
-    def __str__(self):
-
-        return f"Número do Pedido: {self.nmr_pedido}, Cliente: {self.nome_cliente}, Itens: {self.itens_pedido}, Valor Total: {self.valor_total}"
 class FilaDePedidos:
     def __init__(self):
-        self.lista_pedidos = []
+        self.filaPedidos = []
         
-    def adicionar_pedido(self, nmr_pedido, nome_cliente, itens_pedido, valor_total):
-        novo_pedido = Pedido(nmr_pedido, nome_cliente, itens_pedido, valor_total)
-        self.lista_pedidos.append(novo_pedido)
+    def adicionar_pedido(self,numero,nome, prato):
+        novo_pedido = Pedido(numero, nome, prato)
+        self.filaPedidos.append(novo_pedido)
         
+    def remover_pedido(self):
+        if len(self.filaPedidos) < 1:
+            return None
+        return self.filaPedidos.pop(0)
+    
+    def mostrar_pedidos(self):
+        if len(self.filaPedidos) < 1:
+            print("Não há pedidos no momento")
+            return
+        for pedido in self.filaPedidos:
+            print(f"Número: {pedido.numero}, Nome:{pedido.nome}, Prato:{pedido.prato}")
+            
+            
+fila = FilaDePedidos()
 
-    def remover_pedido(self,nmr_pedido):
-        for pedido in self.lista_pedidos:
-            if pedido.nmr_pedido == nmr_pedido:
-                self.lista_pedidos.remove(pedido)
-                return
-            print("O pedido não está na lista")
+fila.adicionar_pedido(1, "João", "Hambúrguer")
+fila.adicionar_pedido(2, "Maria", "Pizza")
+fila.adicionar_pedido(3, "José", "Sushi")
 
-    def listar_pedidos(self):
-        print("Lista de pedidos:")
-        for pedido in self.lista_pedidos:
-            print(pedido)
+fila.mostrar_pedidos()
 
+pedidoAtendido = fila.remover_pedido()
+print(f"Pedido atendido: Número {pedidoAtendido.numero}, Prato: {pedidoAtendido.prato}")
 
-
-
-
-
-
-
-
-pedido = FilaDePedidos()
-pedido.adicionar_pedido(1,"Roberto", "Bolonhesam, Bife, Frango", "R$18")
-pedido.adicionar_pedido(2,"Roberto", "Bolonhesam, Bife, Frango", "R$18")
-pedido.adicionar_pedido(3,"Roberto", "Bolonhesam, Bife, Frango", "R$18")
-pedido.adicionar_pedido(4,"Roberto", "Bolonhesam, Bife, Frango", "R$18")
-pedido.adicionar_pedido(5,"Roberto", "Bolonhesam, Bife, Frango", "R$18")
-
-pedido.listar_pedidos()
+fila.mostrar_pedidos()
