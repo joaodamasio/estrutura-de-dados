9383b54588e90ae260aad4c352c6296b1af2ec74
Messias <125613915+joaodamasio@users.noreply.github.com>
2024-04-29 19:25:48 -0300

    refatorando o codigo

--- BuscaBinaria.py
+++ BuscaBinaria.py
@@ -1,13 +1,34 @@
-def busca_binaria(array, item, inicio=0, fim=None):
-    if fim is None:
-        fim = len(array)-1
-    if inicio <= fim:
-        m = (inicio + fim)//2
-        if array[m] == item:
-            return m
-        if item < array[m]:
-            return busca_binaria(array, item, inicio, m-1)
+def busca_binaria(array, alvo):
+    inicio = 0
+    fim = len(array) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+
+        if array[meio] == alvo:
+            return meio
+        elif array[meio] < alvo:
+            inicio = meio + 1
         else:
-            return busca_binaria(array, item, inicio, m+1, fim)
-        
-    return None
+            fim = meio - 1
+
+    return -1
+
+def ler_array():
+    array = input("Digite os elementos do array separados por espaço: ").split()
+    return [int(x) for x in array]
+
+def ler_alvo():
+    return int(input("Digite o elemento que deseja buscar: "))
+
+def main():
+    array = ler_array()
+    alvo = ler_alvo()
+    resultado = busca_binaria(array, alvo)
+    if resultado != -1:
+        print(f'O elemento {alvo} foi encontrado no índice {resultado}.')
+    else:
+        print(f'O elemento {alvo} não foi encontrado no array.')
+
+if __name__ == "__main__":
+    main()
