56e2e2b17c3f9e3532359a3e7b0984f4a714bb78
Messias <125613915+joaodamasio@users.noreply.github.com>
2024-04-29 19:36:17 -0300

    refatorando o codigo

--- ArvoreDeBuscaBinaria.py
+++ ArvoreDeBuscaBinaria.py
@@ -3,18 +3,16 @@ class Produto:
         self.id = id
         self.nome = nome
         self.qtd_estoque = qtd_estoque
-        
-        
+
 class Node:
     def __init__(self, produto):
         self.esquerda = None
         self.direita = None
         self.produto = produto
-    
+
 class ArvoreProduto:
     def __init__(self):
         self.raiz = None
-        
         
     def inserir(self, id, nome, qtd):
         produto = Produto(id, nome, qtd)
@@ -22,7 +20,6 @@ class ArvoreProduto:
             self.raiz = Node(produto)
         else:
             self._inserir(produto, self.raiz)
-    
     
     def _inserir(self, produto, node):
         if produto.id < node.produto.id:
@@ -38,28 +35,43 @@ class ArvoreProduto:
         else:
             node.produto = produto
     
-    
     def buscar(self, id):
-        return self._buscar(id,self.raiz)
-    
+        return self._buscar(id, self.raiz)
     
     def _buscar(self, id, node):
         if node is None or node.produto.id == id:
             return node
-        
         elif id < node.produto.id:
             return self._buscar(id, node.esquerda)
         else:
             return self._buscar(id, node.direita)
-        
-        
-arvore = ArvoreProduto()
 
-arvore.inserir(1, 'Camiseta', 10)
-arvore.inserir(2, 'Vestido', 20)
-arvore.inserir(3, 'Tenis', 5)
-produtoBuscado1 = arvore.buscar(2)
-print(produtoBuscado1.produto.nome) 
+def interacao_usuario():
+    arvore = ArvoreProduto()
+    
+    while True:
+        print("\n1. Inserir produto")
+        print("2. Buscar produto por ID")
+        print("3. Sair")
+        escolha = input("Escolha uma opção: ")
+        
+        if escolha == "1":
+            id = int(input("Digite o ID do produto: "))
+            nome = input("Digite o nome do produto: ")
+            qtd = int(input("Digite a quantidade em estoque: "))
+            arvore.inserir(id, nome, qtd)
+            print("Produto inserido com sucesso!")
+        elif escolha == "2":
+            id = int(input("Digite o ID do produto que deseja buscar: "))
+            produto_encontrado = arvore.buscar(id)
+            if produto_encontrado:
+                print(f"Produto encontrado - Nome: {produto_encontrado.produto.nome}, Quantidade em Estoque: {produto_encontrado.produto.qtd_estoque}")
+            else:
+                print("Produto não encontrado.")
+        elif escolha == "3":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha novamente.")
 
-produtoBuscado2 = arvore.buscar(20)
-print(produtoBuscado2) 
+interacao_usuario()
